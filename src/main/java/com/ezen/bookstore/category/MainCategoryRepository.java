f5ecd0fdf32e20f91ef3e966e3af10d41f630b7e
Hyeonjeong <lucy2166@naver.com>
2024-07-11 19:06:42 +0900

    1

--- src/main/java/com/ezen/bookstore/category/MainCategoryRepository.java
+++ src/main/java/com/ezen/bookstore/category/MainCategoryRepository.java
@@ -3,4 +3,6 @@ package com.ezen.bookstore.category;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MainCategoryRepository extends JpaRepository<MainCategory, Long> {
+
+
 }
